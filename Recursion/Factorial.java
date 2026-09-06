2c1500cc62c1396d12e284d085ce81a56d1718ef
devanmanigoli <devanmanigoli@gmail.com>
2026-09-06 21:36:34 +0530

    Added factorial using recursion

---
+++ Recursion/Factorial.java
@@ -0,0 +1,19 @@
+import java.util.Scanner;
+public class Factorial {
+    public static int factorial(int n){
+        if(n==0){
+            return 1;
+
+        }
+        return n*factorial(n-1);
+    } 
+    public static void main(String[]args){
+        Scanner sc=new Scanner(System.in);
+        System.out.println("Enter factorial number:");
+
+        int n=sc.nextInt();
+        factorial(n);
+        System.out.println(factorial(n));
+
+    }
+}
