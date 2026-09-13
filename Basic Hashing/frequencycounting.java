0442ea8752cd99109d54a03845ad7236388b8205
devanmanigoli <devanmanigoli@gmail.com>
2026-09-13 12:44:17 +0530

    Add frequency counting using hashing

---
+++ Basic Hashing/frequencycounting.java
@@ -0,0 +1,33 @@
+import java.util.Scanner;
+
+public class frequencycounting {
+
+    public static void main(String[] args) {
+
+        Scanner sc = new Scanner(System.in);
+
+        System.out.println("Enter size of the Array:");
+        int n = sc.nextInt();
+
+        int[] arr = new int[n];
+
+        System.out.println("Enter the Array Elements:");
+        for (int i = 0; i < n; i++) {
+            arr[i] = sc.nextInt();
+        }
+
+        int[] hash = new int[13];
+
+        for (int i = 0; i < n; i++) {
+            hash[arr[i]] += 1;
+        }
+
+        System.out.println("Enter the queries:");
+        int q = sc.nextInt();
+
+        while (q-- != 0) {
+            int number = sc.nextInt();
+            System.out.println(hash[number]);
+        }
+    }
+}
